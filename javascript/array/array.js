cecc55002d9da999700e12cb2edc21d293d44581
Prem Kathayat <premkathayat13@gmail.com>
2025-12-28 19:30:42 +0545

    add array basic

---
+++ javascript/array/array.js
@@ -0,0 +1,43 @@
+//  Array
+
+// let arr=[1,2,3,4,5,50,4,3,2,3,52]
+
+// console.log(arr)
+// console.log(arr.length)
+// console.log(arr[3])
+// console.log(arr[arr.length-1])  //last index
+
+// let arr=[1,2,3,4,5,50,40,3,20,3,52]
+
+// for(let i=0; i<arr.length;i++){
+//   console.log(arr[i])
+// }
+
+// let sum=0
+//  let arr=[1,2,3,4,5]
+// for(let i=0; i<arr.length;i++){
+//   sum+=arr[i]
+// }
+// console.log(sum)
+
+// let arr=[1,2,3,4,5,50,4,3,2,3,52]
+// for(let i=0; i<arr.length;i++){
+//   if(arr[i]%2==0){
+//   console.log(arr[i])
+//   }
+// }
+
+// let arr=[1,2,3,4,5,50,4,3,2,3,52]
+// for(let  i=arr.length-1;i>=0;i--){
+
+//   console.log(arr[i])
+//   }
+
+// let sum=0
+// let arr=[1,2,3,4,5]
+// for(let i=0; i<arr.length;i++){
+//   if(arr[i]%2==0){
+//   sum+=arr[i]
+//   }
+// }
+// console.log(sum)
