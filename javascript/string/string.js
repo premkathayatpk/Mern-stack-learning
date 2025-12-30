30017aa79312339dc35e626457e9d868137f5929
Prem Kathayat <premkathayat13@gmail.com>
2025-12-30 21:49:32 +0545

    add js string

---
+++ javascript/string/string.js
@@ -0,0 +1,57 @@
+// String
+// let fullName='Sipalaya Info Tech'
+
+// console.log(fullName)
+// console.log(fullName.length)
+// console.log(fullName[0])
+// console.log(fullName[1])
+
+// for(let i=0;i<fullName.length;i++){
+//   console.log(fullName[i])
+// }
+
+// let firstName='Sipalaya'
+// let lastName='Info Tech'
+// console.log(firstName+" "+lastName)
+// console.log(firstName.concat(" "+lastName))
+
+// let fullName='Sipalaya Info Tech'
+// console.log(fullName.toUpperCase())
+// console.log(fullName.toLowerCase())
+// console.log(fullName.startsWith('S'))
+// console.log(fullName.endsWith('h'))
+// console.log(fullName.startsWith('Sipalaya'))
+// console.log(fullName.endsWith('Tech'))
+
+// trim
+// let fullName='  Sipalaya Info Tech  '
+// console.log(fullName)
+// console.log(fullName.trim())
+
+// let fullName='Sipalaya Info Tech'
+// console.log(fullName.replace("S","T"))
+// console.log(fullName.replace('Sipalaya',"Y"))
+// console.log(fullName.replace('a','m'))
+// console.log(fullName.replaceAll('a','b'))
+
+// console.log(fullName.slice(0,8))
+// console.log(fullName.slice(9,13))
+
+// console.log(fullName.charAt(0))
+
+// let fullName='Sipalaya Info Tech'
+// console.log(fullName.split())
+// console.log(fullName.split(''))
+// console.log(fullName.split(' '))
+// let charArr=fullName.split(' ')
+// console.log(charArr[0])
+// console.log(charArr[1])
+// console.log(charArr[2])
+// console.log(fullName.split(' ')[0])
+// console.log(fullName.split(' ')[1])
+// console.log(fullName.split(' ')[2])
+
+
+// let token='hello 2skahfosja3iokjfasio3'
+// console.log(token.split(' ')[1])
+
