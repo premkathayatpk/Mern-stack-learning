29a4948675c087a4c8490b9fb5386c32faa6393d
Prem Kathayat <premkathayat13@gmail.com>
2025-12-31 19:20:21 +0545

    add string task qn 1-10

--- javascript/string/task.js
+++ javascript/string/task.js
@@ -52,5 +52,53 @@ console.log("Num of vowel letter ",count)
 
 
 // 6. Write a program that uses a loop to concatenate the first letter of each word in the string `"Hello EveryOne"` to form a new string
+let splitStr=str.split(" ")
+let newStr=""
+for(let i=0;i<splitStr.length;i++){
+  newStr+=splitStr[i].slice(0,1)
+}
+console.log("concatenate the first letter of each word: ",newStr)
 
+// 7. Create a program that checks if a string is a palindrome (reads the same backward as forward). Use `if else` statements. 
+let s='radar'
+let rev=''
+for(let i=s.length-1;i>=0;i--){
+  // console.log(n[i])
+  rev+=s[i]
+}
+console.log(rev)
+if(s==rev){
+  console.log(s+" is palendrom ")
+}
 
+// 8. Write a program that trims any extra spaces at the beginning and end of a string and then checks if the string is empty. 
+ let fullName='  Sipalaya Info Tech  '
+console.log(fullName)
+console.log(fullName.trim())
+
+// 9. Using a loop, write a program that converts every letter in a string to lowercase if it’s uppercase, and to uppercase if it’s lowercase. 
+convertedStr=""
+for(let i=0;i<str.length;i++){
+  let char=str[i]
+  if (char >= 'a' && char <= 'z') {
+    convertedStr += char.toUpperCase();
+  } 
+  else if (char >= 'A' && char <= 'Z') {
+    convertedStr += char.toLowerCase();
+  } 
+  else {
+    convertedStr += char;
+  }
+
+}
+console.log(convertedStr)
+
+// 10. Write a program that slices the first 5 characters of a string and checks if this substring is equal to `"Hello"`. 
+
+let sliceStr=str.slice(0,5)
+if("Hello"==sliceStr){
+  console.log(`substring is equal to "Hello"`)
+}
+else{
+  console.log(`substring is not equal to "Hello"`)
+}
