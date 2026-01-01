62dbbf824240cc63aae4da55ecd20487772576f8
Prem Kathayat <premkathayat13@gmail.com>
2026-01-01 21:41:33 +0545

    add promise

---
+++ javascript/promise.js
@@ -0,0 +1,21 @@
+//promise
+
+// promise create
+
+// const p = new Promise((resolve, reject) => {
+//   let isRead = false;
+//   if (isRead) {
+//     resolve("Data found");
+//   } else {
+//     reject("Data not found");
+//   }
+// });
+
+// //promise handling
+// p.then((data) => {
+//   console.log(data);
+// }).catch((err) => {
+//   console.log(err);
+// });
+
+//
