48402f955dc5269771146dfac251382f4b29a528
Prem Kathayat <premkathayat13@gmail.com>
2026-01-01 21:39:42 +0545

    add error handling

---
+++ javascript/errorHandling.js
@@ -0,0 +1,34 @@
+//Eror
+//  try{
+//   console.log(x)
+//  }catch(error){
+//   // console.log(error)
+//   console.log("x is not define")
+//  }
+//  finally{
+//   console.log('finally')
+
+//  }
+
+//  console.log(500)
+
+
+// types of error
+// compile time error
+// 1. syntax error
+
+//Runtime error
+// 2. Refrencee error
+// try{
+//   console.log(x)
+// }catch(error){
+//   console.log(error)
+// }
+
+// // 3. typeError
+// try{
+//   let x=10
+//   console.log(x.toUpperCase())
+// }catch(error){
+//   console.log(error)
+// }
