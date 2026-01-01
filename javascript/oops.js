04f5b80477ec9199cd899e0b7e095b4f1e27a2b3
Prem Kathayat <premkathayat13@gmail.com>
2026-01-01 21:40:51 +0545

    add basic  oops consept

---
+++ javascript/oops.js
@@ -0,0 +1,52 @@
+//OOPs
+//dry=do not repate yourself
+
+// class User {
+//   constructor(uName, uPhn) {
+//     this.name = uName;
+//     this.phn = uPhn;
+//   }
+//   walk=()=>{
+//     console.log("walk")
+//   }
+//   info=()=>{
+//     console.log(this.name)
+//     console.log(this.phn)
+//   }
+// }
+
+// const u1 = new User("Ram", 9800000000);
+// const u2 = new User("Sita", 9811000000);
+
+// // console.log(u1);
+// // console.log(u2);
+// // u1.walk()
+// u1.info()
+
+
+//Interface
+
+// class Person{
+//   constructor(name,age){
+//     this.name=name
+//     this.age=age
+//   }
+// }
+// class Student extends Person{
+//   constructor(name,age,roll){
+//   super(name,age)
+//   this.roll=roll
+//   }
+// }
+
+// class Teacher extends Person{
+//   constructor(name,age,dep){
+//   super(name,age)
+//   this.dep=dep
+//   }
+// }
+// const s1=new Student("Ram",22,1)
+// console.log(s1)
+// const t1=new Teacher("abc",50,'IT')
+// console.log(t1)
+
