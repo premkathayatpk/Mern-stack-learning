ac31204330f4fe0c7348bccadbabc770fccf9878
Prem Kathayat <premkathayat13@gmail.com>
2026-01-02 20:19:22 +0545

    add dom

---
+++ javascript/dom/script.js
@@ -0,0 +1,25 @@
+// console.log(document)
+
+//selectors
+//id
+//class
+//tagName
+
+
+let h1=document.getElementsByTagName('h1')
+console.log(h1[0].innerHTML)
+console.log(h1[1].innerHTML)
+
+// DOM= Document Object Model
+
+
+// document={
+// html:{
+//   head:{
+//     title:"DOM"
+//   }
+//   body:{
+//     h1:"DOM"
+//   }
+// }
+// }
