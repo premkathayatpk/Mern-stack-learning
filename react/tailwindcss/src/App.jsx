c143366849e38bb9c6fe58a73496a92ce7d537f7
Prem Kathayat <premkathayat13@gmail.com>
2026-01-20 20:48:29 +0545

    react-icon

--- react/tailwindcss/src/App.jsx
+++ react/tailwindcss/src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
 
 const App = () => {
-  return <div className="border-4 mt-10 m-auto p-10 border-red-600 h-96 w-96">App</div>;
+  return (
+    <>
+      <div className="nav py-3 px-10 bg-black text-white flex justify-between items-center">
+        <div className="logo font-bold text-2xl flex">
+          <FcGoogle size={30}/>
+          <h1>Logo</h1>
+        </div>
+        <div>
+          <ul  className="text-gray-300 flex gap-3 justify-center list-none">
+            <li>Services</li>
+            <li>Products</li>
+            <li>About</li>
+          </ul>
+        </div>
+        <div className="contact">
+          <button className="bg-cyan-400 rounded-lg p-1 w-20">Contact</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
