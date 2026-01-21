92376f702cd55c510f38f7d2502b4d6806d6b0f3
Prem Kathayat <premkathayat13@gmail.com>
2026-01-21 12:36:44 +0545

    add portfolio using tailwind css

---
+++ react/portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideNav from './components/SideNav'
+import Main from './components/Main'
+
+const App = () => {
+  return (
+    <div className='flex '>
+      <SideNav/>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
---
+++ react/portfolio/src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='bg-blue-900 w-96'>
+      <div className="banner">
+
+      </div>
+      <div className="aboutMe">
+        <h1>Get To Know</h1>
+        <h1>About Me</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Main
